faf28cde279ddc38d98401bcc6a0144144b9c91f
Abhijith N B <www.abhijithbose@gmail.com>
2023-04-24 21:37:57 +0530

    passed summary and payment as an array

--- promise.js
+++ promise.js
@@ -9,22 +9,22 @@ console.log("Start");
 
 createOrder(cart)
     .then((orderId) => {
-        console.log("Order ID = "+orderId);
+        console.log("Order ID = " + orderId);
         return orderId;
     })
     .then((orderId) => {
         return proceedToPayment(orderId);
     })
     .then((payment) => {
-        console.log("Total Payment = "+payment);
+        console.log("Total Payment = " + payment);
         return showOrderSummary(payment);
     })
-    .then((summary,payment) => {
+    .then(([summary, payment]) => {
         console.log(summary);
-        return updateWallet(summary,payment);
+        return updateWallet(summary, payment);
     })
     .then((wallet) => {
-        console.log("Wallet Balance: Rs."+wallet);
+        console.log("Wallet Balance: Rs." + wallet);
     })
     .catch((err) => {
         console.log(err.message);
@@ -40,9 +40,9 @@ function createOrder(cart) {
         }
         const orderId = "12345";
         if (orderId) {
-            setTimeout(function () {
-                resolve(orderId);
-            }, 1000);
+            // setTimeout(function () {
+            resolve(orderId);
+            // }, 1000);
         }
     });
 }
@@ -61,14 +61,14 @@ function proceedToPayment(orderId) {
 
 function showOrderSummary(payment) {
     return new Promise((resolve, reject) => {
-        const summary = "payment of "+payment+" Successfull";
+        const summary = "payment of " + payment + " Successfull";
         if (payment) {
-            resolve(summary,payment);
+            resolve([summary, payment]);
         }
     });
 }
 
-function updateWallet(summary,payment) {
+function updateWallet(summary, payment) {
     return new Promise((resolve, reject) => {
         wallet = wallet[0] - payment[0];
         if (summary) {
